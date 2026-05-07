4468d4212ba69d210e98fe5c8ee6439106d8b2d4
Raghul S <rexizzz1815@gmail.com>
2026-05-07 22:47:52 +0530

    Implement encoding and decoding for strings

---
+++ Encode and Decode Strings.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Codec {
+
+    public String encode(List<String> strs) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(String s : strs) {
+            sb.append(s.length()).append("#").append(s);
+        }
+
+        return sb.toString();
+    }
+
+    public List<String> decode(String s) {
+
+        List<String> result = new ArrayList<>();
+        int i = 0;
+
+        while(i < s.length()) {
+
+            int j = i;
+
+            while(s.charAt(j) != '#') {
+                j++;
+            }
+
+            int length = Integer.parseInt(s.substring(i, j));
+
+            i = j + 1;
+
+            result.add(s.substring(i, i + length));
+
+            i = i + length;
+        }
+
+        return result;
+    }
+}
