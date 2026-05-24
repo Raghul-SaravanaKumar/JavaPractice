6e9ae8dfe40db62b6e1457e77ebb56766887d76e
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-05-24 22:00:57 +0530

    Implement minimum distance calculation in BST

---
+++ Minimum Distance Between BST Nodes.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    int min = Integer.MAX_VALUE;
+    TreeNode prev = null;
+
+    public int minDiffInBST(TreeNode root) {
+
+        inorder(root);
+        return min;
+    }
+
+    private void inorder(TreeNode node) {
+
+        if(node == null) {
+            return;
+        }
+
+        inorder(node.left);
+
+        if(prev != null) {
+
+            min = Math.min(min,
+                    node.val - prev.val);
+        }
+
+        prev = node;
+
+        inorder(node.right);
+    }
+}
