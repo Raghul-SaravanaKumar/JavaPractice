4ac36bf0622b0c2d42d619399a3eacf92b5d66b3
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-05-24 22:00:24 +0530

    Add solution for checking alternating bits in binary

---
+++ Binary Number with Alternating Bits.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public boolean hasAlternatingBits(int n) {
+
+        int prev = n % 2;
+        n /= 2;
+
+        while(n > 0) {
+
+            int current = n % 2;
+
+            if(current == prev) {
+                return false;
+            }
+
+            prev = current;
+            n /= 2;
+        }
+
+        return true;
+    }
+}
