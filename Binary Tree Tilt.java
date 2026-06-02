1f037a2988ac84b2770d2fc6aa372a3066598562
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-06-02 23:35:52 +0530

    Implement Binary Tree Tilt calculation

---
+++ Binary Tree Tilt.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    int tilt = 0;
+
+    public int findTilt(TreeNode root) {
+
+        sum(root);
+        return tilt;
+    }
+
+    private int sum(TreeNode node) {
+
+        if(node == null) {
+            return 0;
+        }
+
+        int left = sum(node.left);
+        int right = sum(node.right);
+
+        tilt += Math.abs(left - right);
+
+        return left + right + node.val;
+    }
+}
