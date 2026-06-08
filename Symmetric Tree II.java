254422208fdfcced0ad66d0b09241aebcbc2ca02
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-06-08 21:24:21 +0530

    Implement isSymmetric method for tree symmetry check

---
+++ Symmetric Tree II.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+
+        return mirror(root.left, root.right);
+    }
+
+    private boolean mirror(TreeNode a, TreeNode b) {
+
+        if(a == null && b == null) {
+            return true;
+        }
+
+        if(a == null || b == null) {
+            return false;
+        }
+
+        return a.val == b.val &&
+               mirror(a.left, b.right) &&
+               mirror(a.right, b.left);
+    }
+}
