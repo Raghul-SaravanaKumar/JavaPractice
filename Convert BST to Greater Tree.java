4d045f857372271938e4ec283faf3270a862d51d
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-06-11 21:54:47 +0530

    Implement conversion of BST to Greater Tree

---
+++ Convert BST to Greater Tree.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+
+        traverse(root);
+        return root;
+    }
+
+    private void traverse(TreeNode node) {
+
+        if(node == null) {
+            return;
+        }
+
+        traverse(node.right);
+
+        sum += node.val;
+        node.val = sum;
+
+        traverse(node.left);
+    }
+}
