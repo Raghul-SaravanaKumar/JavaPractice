3ebd389ef754cd3f1ad72d45e86a3e391089ac34
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-06-12 21:24:38 +0530

    Implement method to find words from keyboard rows

---
+++ Keyboard Row.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+
+    public String[] findWords(String[] words) {
+
+        String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+
+        List<String> result = new ArrayList<>();
+
+        for(String word : words) {
+
+            String w = word.toLowerCase();
+
+            String row = row1.contains("" + w.charAt(0))
+                       ? row1
+                       : row2.contains("" + w.charAt(0))
+                       ? row2
+                       : row3;
+
+            boolean valid = true;
+
+            for(char c : w.toCharArray()) {
+
+                if(!row.contains("" + c)) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if(valid) {
+                result.add(word);
+            }
+        }
+
+        return result.toArray(new String[0]);
+    }
+}
