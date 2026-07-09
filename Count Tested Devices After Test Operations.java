eec0d8cee8571f3c71967caa234a0685957be191
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-07-09 21:47:27 +0530

    Implement method to count tested devices

---
+++ Count Tested Devices After Test Operations.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int countTestedDevices(int[] batteryPercentages) {
+
+        int tested = 0;
+
+        for(int battery : batteryPercentages) {
+
+            if(battery - tested > 0) {
+                tested++;
+            }
+        }
+
+        return tested;
+    }
+}
