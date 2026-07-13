fba7f8f198db5df9845d6401c050132a3bb85311
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-07-13 22:04:40 +0530

    Implement method to count pairs in an array

---
+++ Maximum Number of Pairs in Array.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public int[] numberOfPairs(int[] nums) {
+
+        int[] freq = new int[101];
+
+        for(int num : nums) {
+            freq[num]++;
+        }
+
+        int pairs = 0;
+
+        for(int count : freq) {
+            pairs += count / 2;
+        }
+
+        return new int[]{pairs, nums.length - pairs * 2};
+    }
+}
