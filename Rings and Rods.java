7c280530b3f0efaa0a804fd16fc6845151967771
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-07-15 22:10:54 +0530

    Implement countPoints method to count colored rods

---
+++ Rings and Rods.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public int countPoints(String rings) {
+
+        int[] rods = new int[10];
+
+        for(int i = 0; i < rings.length(); i += 2) {
+
+            char color = rings.charAt(i);
+            int rod = rings.charAt(i + 1) - '0';
+
+            if(color == 'R') rods[rod] |= 1;
+            if(color == 'G') rods[rod] |= 2;
+            if(color == 'B') rods[rod] |= 4;
+        }
+
+        int count = 0;
+
+        for(int mask : rods) {
+
+            if(mask == 7) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
