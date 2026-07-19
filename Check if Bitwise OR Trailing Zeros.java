1260872cb8de8b0e5fef1db01819d877c955392e
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-07-19 22:42:52 +0530

    Add method to check for trailing zeros in Bitwise OR

---
+++ Check if Bitwise OR Trailing Zeros.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public boolean hasTrailingZeros(int[] nums) {
+
+        int evenCount = 0;
+
+        for(int num : nums) {
+
+            if(num % 2 == 0) {
+                evenCount++;
+            }
+
+            if(evenCount >= 2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
