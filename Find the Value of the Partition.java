cf65ca34aa9c3996547a3633915694dbed3f127c
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-07-24 22:05:30 +0530

    Implement findValueOfPartition method in Solution class

---
+++ Find the Value of the Partition.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+
+    public int findValueOfPartition(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int ans = Integer.MAX_VALUE;
+
+        for(int i = 1; i < nums.length; i++) {
+
+            ans = Math.min(ans, nums[i] - nums[i - 1]);
+        }
+
+        return ans;
+    }
+}
